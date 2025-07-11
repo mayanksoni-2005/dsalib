29ff17ac86eb8e8507ed8b267a5d94ae21bc433b
mayanksoni-2005 <167397822+mayanksoni-2005@users.noreply.github.com>
2025-07-11 19:52:31 +0530

    Create maydsa.hpp

---
+++ include/maydsa.hpp
@@ -0,0 +1,11 @@
+#ifndef MAYDSA_HPP
+#define MAYDSA_HPP
+
+// All core data structures
+#include "vector.hpp"
+#include "linked_list.hpp"
+#include "heap.hpp"
+#include "graph.hpp"
+#include "dsu.hpp"    
+
+#endif // MAYDSA_HPP
